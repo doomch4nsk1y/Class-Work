aa7e441e288cbfa4174f45ef6707dc0cb4389d86
doomch4nsk1y <157492031+doomch4nsk1y@users.noreply.github.com>
2024-02-02 11:45:34 +0300

    Практическая задача 2 02.02.2024

---
+++ Практическая задача 2.py
@@ -0,0 +1,20 @@
+a = 8
+b = float(5.7)
+# and
+print(a > 3 and b < 10)
+print(a != 10 and b ==7)
+print(a >= 9 and b <= 6)
+print(a <= 9 and b != 6)
+
+# or
+print(a == 8 or b > 6)
+print(a <= 7 or b <=5)
+print(a != 9 or b >= 3)
+print(a == 3 or b < 2)
+
+# str
+str1 = "a"
+str2 = "b"
+lol = str1 > "b" and str2 < "c"
+print(lol)
+print(str1 != 4 and str2 == 12)
