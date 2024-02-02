98b658e968ee9a8d4d5b1c88ecbf0b62b2e57ba6
doomch4nsk1y <157492031+doomch4nsk1y@users.noreply.github.com>
2024-02-02 11:05:47 +0300

    Практическая задача 02.02.2024

---
+++ Практическая.py
@@ -0,0 +1,16 @@
+#Задачка
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+
+big_netseloe = tseloe * 3.5
+print(big_netseloe)
+
+drobnoe = drobnoe - 1
+print(drobnoe)
+
+print(tseloe / drobnoe)
+print(big_netseloe / drobnoe)
+
+print(stroka * 2 +"_" + "Yes" * 3)
+
