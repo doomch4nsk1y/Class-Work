3ff5ca0a3685bbb8335f73a5a8a40e8914e4fb35
doomch4nsk1y <157492031+doomch4nsk1y@users.noreply.github.com>
2024-04-02 16:01:21 +0300

    02.04.2024

---
+++ Задачка 2.py
@@ -0,0 +1,19 @@
+#Задача 2 
+def calculate_area(length, width):
+    area = dlina * shirina
+    return area
+def calculate_perimeter(dlina, shirina):
+    perimeter = 2 * (dlina + shirina)
+    return perimeter
+def print_rectangle_properties(area, perimeter):
+    print("Площадь прямоугольника:", area)
+    print("Периметр прямоугольника:", perimeter)
+length = 5
+width = 10
+rectangle_a = calculate_area(length, width)
+rectangle_p = calculate_perimeter(length, width)
+print_r(rectangle_area, rectangle_perimeter)
+if rectangle_a > 30:
+    print("Площадь прямоугольника больше 30")
+else:
+    print("Площадь прямоугольника меньше или равна 30")
